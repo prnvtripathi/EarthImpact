1aae9b3334d88e4dde1187f2482624a4f2a86d2d
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-04-11 19:04:44 +0530

    0.0.2

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,25 @@
-import express from "express"
-import bodyParser from "body-parser"
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
 
-app.listen(process.env.port||3000, async ()=>{
-    console.log("Server Started on port 3000")
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.post('/submit', (req, res) => {
+
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+
+  
+  console.log(`Received form submission: ${name}, ${email}, ${message}`);
+
+  res.send('Thanks for submitting the form!');
+});
+
+app.listen(5500, () => {
+  console.log('Server is listening on port 3000');
+});
