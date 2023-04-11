29c6a242b840198f3c5dceb1d7376a39b0d29c7a
Pranav Tripathi <tripathipranav14@gmail.com>
2023-04-11 15:13:49 +0530

    remove tailwind create layout navbar

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,19 @@
 import Link from 'next/link'
 import './globals.css'
+import './page.module.css'
+import { Inter } from 'next/font/google'
+import {FaGithub} from 'react-icons/fa'
+
+const inter = Inter({
+  subsets: ['latin']
+})
 
 export default function RootLayout({ children }) {
-
-
   return (
     <html lang="en">
       <body>
-        <nav>
-          <div className='nav-brand'>
+        <nav className={inter.className}>
+          <div className='brand-container'>
             <Link href='/' className='brand'>CarbonCheck</Link>
           </div>
           <ul>
@@ -16,6 +21,7 @@ export default function RootLayout({ children }) {
             <li><Link href='/about' className='nav-links'>About</Link></li>
             <li><Link href='/contact' className='nav-links'>Contact</Link></li>
           </ul>
+          <Link className='github-link' href='https://github.com/prnvtripathi/carbon-check'><FaGithub /> Github</Link>
         </nav>
         {children}
       </body>
