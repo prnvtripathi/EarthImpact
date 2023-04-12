e8d7831580bb9d50fb86f7258ecccebd0e0c40f4
Pranav Tripathi <tripathipranav14@gmail.com>
2023-04-13 01:15:16 +0530

    Create save page

---
+++ app/save/accordion.js
@@ -0,0 +1,20 @@
+"use client"
+
+import '../globals.css';
+import { useState } from 'react';
+
+const Accordion = ({ title, content }) => {
+    const [isActive, setIsActive] = useState(false);
+
+    return (
+        <div className="accordion-item">
+            <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+                <div>{title}</div>
+                <div>{isActive ? '-' : '+'}</div>
+            </div>
+            {isActive && <div className="accordion-content">{content}</div>}
+        </div>
+    );
+};
+
+export default Accordion;
