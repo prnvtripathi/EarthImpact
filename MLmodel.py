0148228a097c1c0087b025849ecbc6ba2d18204a
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-04-13 00:31:09 +0530

    0.0.5

--- MLmodel.py
+++ MLmodel.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_json('data.json')
+dataset = pd.read_json('../backend/data.json')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
