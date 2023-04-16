aa64fffeab847a2a67691153ad6faa2da75a45d1
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-04-16 10:18:28 +0530

    0.0.10

--- discord bot/responses.py
+++ discord bot/responses.py
@@ -5,11 +5,11 @@ def handle_response(message):
     if p_message == 'task':
         return '[1: Car pooled, 2: Walk to the work, 3: Using Low Natural gasses]'
     if p_message == 'task 1 completed':
-        return "concratulations you leveled up"
+        return "Congratulations you leveled up👍🥳"
     if p_message == 'task 2 completed':
-        return "concratulations you leveled up"
+        return "Congratulations you leveled up👍🥳"
     if p_message == 'task 3 completed':
-        return "concratulations you leveled up"
+        return "Congratulations you leveled up👍🥳"
     
     #if p_message == 'roll':
         #return str(random.randint(1,10))
